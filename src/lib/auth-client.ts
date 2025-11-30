a5e7b77cf4ba972fcf6d7c595a23f476744531af
derhyben-lang <derhyben@gmail.com>
2025-11-30 05:59:14 +0100

    Update refetch method to return void

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -57,5 +57,5 @@ export function useSession(): SessionData {
       fetchSession();
    }, []);
 
-   return { data: session, isPending, error, refetch };
+   return { data: session, isPending, error, refetch: () => void refetch() };
 }
